a1418df19f2f092d8bda153e963568466e8d47a2
Peter Campbell <peterhcampbell06@gmail.com>
2023-10-11 13:49:18 -0600

    plr

--- KaggleAEAEDA.R
+++ KaggleAEAEDA.R
@@ -49,6 +49,59 @@ vroom_write(x=amazon_predictions_lr, file="./lr.csv", delim=",")
 
 
 
+###################################
+## Penalized Logistic Regression ##
+###################################
+
+my_recipe_plr <- recipe(ACTION ~., data = empl_access_train) %>%
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>%
+  step_other(all_factor_predictors(), threshold = .001) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>%
+  prep()
+
+my_mod_plr <- logistic_reg(mixture = tune(), penalty = tune()) %>%
+  set_engine("glmnet")
+
+plr_workflow <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(my_mod_plr)
+
+
+tuning_grid_plr <- grid_regular(penalty(),
+                                mixture(),
+                                levels = 10)
+
+folds <- vfold_cv(empl_access_train, v = 5, repeats = 1)
+
+CV_results <- plr_workflow %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid_plr,
+            metrics=metric_set(roc_auc))
+
+
+best_tune_plr <- CV_results %>%
+  select_best("roc_auc")
+
+
+finalize_workflow_plr <- plr_workflow %>%
+  finalize_workflow(best_tune_plr) %>%
+  fit(data = empl_access_train)
+
+## Predictions
+
+plr_predictions <- predict(finalize_workflow_plr, new_data = empl_access_test, type = "prob")
+
+
+amazon_predictions_plr <- plr_predictions %>%
+  bind_cols(., empl_access_test) %>%
+  select(id, .pred_1) %>%
+  rename(ACTION = .pred_1)
+
+
+vroom_write(x=amazon_predictions_plr, file="./plr.csv", delim=",")
+
+
+
 
 
 
