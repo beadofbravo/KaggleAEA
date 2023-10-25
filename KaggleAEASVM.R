648a3334ace824cba8cc89277873da1d6e7ebd3e
Peter Campbell <peterhcampbell06@gmail.com>
2023-10-25 13:39:42 -0600

    svm

--- KaggleAEASVM.R
+++ KaggleAEASVM.R
@@ -1,4 +1,3 @@
-## setting up libraries
 library(tidyverse)
 library(vroom)
 library(patchwork)
@@ -9,11 +8,11 @@ library(embed)
 library(doParallel)
 library(discrim)
 library(naivebayes)
-
+library(kernlab)
 
 cl <- makePSOCKcluster(8)
 registerDoParallel(cl)
-  
+
 
 empl_access_train <- vroom("./train.csv") %>%
   mutate(ACTION = factor(ACTION))
@@ -22,51 +21,62 @@ empl_access_test <- vroom("./test.csv")
 my_recipe_nb <- recipe(ACTION ~., data = empl_access_train) %>%
   step_mutate_at(all_numeric_predictors(), fn = factor) %>%
   step_other(all_factor_predictors(), threshold = .001) %>%
-  step_dummy(all_nominal_predictors()) %>%
   step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>%
+  step_dummy(all_nominal_predictors()) %>%
+  step_normalize(all_predictors()) %>%
+  step_pca(all_predictors(), threshold = .91) %>%
   prep()
 
-nb_model <- naive_Bayes(Laplace = tune(), smoothness = tune()) %>%
+svmPoly <- svm_poly(degree=tune(), cost=tune()) %>% # set or tune
   set_mode("classification") %>%
-  set_engine("naivebayes")
+  set_engine("kernlab")
 
-nb_workflow <- workflow() %>%
+svmRadial <- svm_rbf(rbf_sigma=tune(), cost=tune()) %>% # set or tune
+  set_mode("classification") %>%
+  set_engine("kernlab")
+
+svmLinear <- svm_linear(cost=tune()) %>% # set or tune
+  set_mode("classification") %>%
+  set_engine("kernlab")
+
+## Fit or Tune Model HERE
+svm_wf <- workflow() %>%
   add_recipe(my_recipe_nb) %>%
-  add_model(nb_model)
+  add_model(svmLinear)
 
-tuning_grid_nb <- grid_regular(Laplace(),
-                                smoothness(),
-                                levels = 5)
+tuning_grid_svm <- grid_regular(cost(),
+                               levels = 5)
+
 
 folds <- vfold_cv(empl_access_train, v = 5, repeats = 1)
 
-nb_results_tune <- nb_workflow %>%
+
+svm_results_tune <- svm_wf %>%
   tune_grid(resamples = folds,
-            grid = tuning_grid_nb,
+            grid = tuning_grid_svm,
             metrics=metric_set(roc_auc))
 
-bestTune <- nb_results_tune %>%
+
+bestTune <- svm_results_tune %>%
   select_best("roc_auc")
 
 
-final_wf_nb <- nb_workflow %>%
+final_wf_svm <- svm_workflow %>%
   finalize_workflow(bestTune) %>%
   fit(data=empl_access_train)
 
-nb_predictions <- final_wf_nb %>%
+svm_predictions <- final_wf_svm %>%
   predict(new_data = empl_access_test, type = "prob")
 
-amazon_predictions_nb <- nb_predictions %>%
+amazon_predictions_svm <- svm_predictions %>%
   bind_cols(., empl_access_test) %>%
   select(id, .pred_1) %>%
   rename(ACTION = .pred_1)
 
 
-vroom_write(x=amazon_predictions_nb, file="./nb.csv", delim=",")
+vroom_write(x=amazon_predictions_nb, file="./svm.csv", delim=",")
 
 stopCluster(cl)
-
-
 
 
 
